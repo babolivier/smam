0c313335032ec0e18cbbf84e032c10d180908db7
Brendan Abolivier <contact@brendanabolivier.com>
2016-10-22 18:48:07 +0200

    Again, temporary wildcard

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(bodyParser.json());
 // that.
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
     next();
 });
 
